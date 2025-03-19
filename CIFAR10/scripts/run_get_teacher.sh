9704e4cae6e0d3437dd1418fffa59cfd9fbec3d1
ieveresthd <ieveresthd@gmail.com>
2025-03-19 15:59:00 +0800

    update

--- CIFAR10/scripts/run_get_teacher.sh
+++
@@ -1,2 +0,0 @@
-#python3 train_teacher.py --arch pyramidnet20 -lr 0.1 --weight-decay 5e-4 --log --save --gpu_id 0
-#python3 train_teacher.py --arch wrn_28_4 -lr 0.1 --weight-decay 5e-4 --log --save --gpu_id 0
